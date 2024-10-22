bc0038bfdce309420509211ee21cb295f2f8b819
Dyron Huapaya <144504783+MaineMa@users.noreply.github.com>
2024-10-21 20:31:21 -0500

    feat: Agregacion de la ED arbol binario con su nodo

--- TPFBCP.cpp
+++ TPFBCP.cpp
@@ -1,16 +1,45 @@
 #include "pch.h"
 #include <stdlib.h>
 #include "Bcp.h"
+#include "ArbolBinario.h"
 
 using namespace System;
 using namespace std;
 
 int main()
 {
-	Bcp* bcp = new Bcp();
-
-	bcp->MenuBCP();
-
+	//Verificacion del arbol aplicado a cuentas bancarias
+	/*CuentaBancaria* aux = new CuentaBancaria(1, 1, "1", "1", 1234);
+	ArbolBinario<CuentaBancaria*>* AB = new ArbolBinario<CuentaBancaria*>(aux, [](CuentaBancaria* a, CuentaBancaria* b)->bool{
+		if (a != nullptr && b != nullptr)
+			return a->getSaldo() < b->getSaldo();
+		else return false;
+	});
+	AB->insertar(new CuentaBancaria(2, 2, "2", "2", 1111));
+	AB->insertar(new CuentaBancaria(3, 3, "3", "3", 2000));
+	AB->insertar(new CuentaBancaria(4, 4, "4", "4", 1222));
+	AB->PreOrden([](CuentaBancaria* a)
+	{
+		a->print();
+	});
 	system("pause");
+	Console::Clear();
+	AB->EnOrden([](CuentaBancaria* a)
+	{
+		a->print();
+	});
+	system("pause");
+	Console::Clear();
+	AB->PostOrden([](CuentaBancaria* a)
+	{
+		a->print();
+	});
+	system("pause");
+	Console::Clear();
+	cout << AB->altura();
+	AB->buscarNodoPorId(4, AB->getRaiz())->dato->print();
+	system("pause");*/
+	Bcp* bcp = new Bcp();
+	bcp->MenuBCP();
     return 0;
 }
