bea237071a25bbb30c799d48857564a4b50cbef1
GilbertHuarcaya <gilberthuarcaya@gmail.com>
2024-11-16 18:06:23 -0500

    Refactorización y mejoras en clases de grafo
    Se ha añadido la directiva `#pragma once` en los archivos `GArista.h`, `GVertice.h` y `Grafo.h` para evitar inclusiones múltiples.
    
    Se han incluido las librerías `iostream` y `vector` en los archivos `GArista.h`, `GVertice.h` y `Grafo.h`.
    
    Se ha añadido la declaración `using namespace std;` en los archivos `GArista.h`, `GVertice.h` y `Grafo.h`.
    
    Se han creado las clases plantilla `GArista` y `GVertice` en sus respectivos archivos con sus atributos y métodos.
    
    Se ha modificado la clase `Grafo` para utilizar las nuevas clases `GVertice` y `GArista`.
    
    Se han eliminado las definiciones de la clase `GArista` del archivo `Grafo.h`.
    
    Se han añadido métodos a la clase `Grafo` para gestionar vértices y aristas, incluyendo métodos para agregar vértices, enlazar vértices de manera dirigida y no dirigida, y obtener datos y aristas por posición.
    
    Se han refactorizado los métodos `esRepetido` y `getPosDato` para mejorar su funcionalidad.

---
+++ GVertice.h
@@ -0,0 +1,59 @@
+#pragma once
+#include "iostream"
+#include "vector"
+#include "GArista.h"
+using namespace std;
+
+template <typename T, typename Q>
+class GVertice
+{
+	T dato;
+	vector<GArista<Q>*>* aristas;
+public:
+	GVertice();
+	GVertice(T dato);
+	void setDato(T dato);
+	T getDato();
+	GArista<Q>* getAristaPorPos(int pos);
+	void agregarArista(GArista<Q>* arista);
+};
+
+template <typename T, typename Q>
+GVertice<T, Q>::GVertice()
+{
+	dato = nullptr;
+	aristas = new vector<GArista<Q>*>;
+}
+
+template <typename T, typename Q>
+GVertice<T, Q>::GVertice(T dato)
+{
+	this->dato = dato;
+	aristas = new vector<GArista<Q>*>;
+}
+
+template <typename T, typename Q>
+void GVertice<T, Q>::setDato(T dato)
+{
+	this->dato = dato;
+}
+
+template <typename T, typename Q>
+T GVertice<T, Q>::getDato()
+{
+	return dato;
+}
+
+template <typename T, typename Q>
+GArista<Q>* GVertice<T, Q>::getAristaPorPos(int pos)
+{
+	if (aristas == nullptr)return nullptr;
+	if (pos < 0)return nullptr;
+	return aristas->size() > pos ? aristas->at(pos) : nullptr;
+}
+
+template <typename T, typename Q>
+void GVertice<T, Q>::agregarArista(GArista<Q>* arista)
+{
+	aristas->push_back(arista);
+}
